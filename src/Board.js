b356a4f874937fafedd142da800183d5244ac5e8
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-07-22 17:27:09 +0100

    add initial board and tests

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,54 @@
 module.exports = class Board {
-  constructor() {}
+  static new() {
+    const threeByThreeTotalSquares = 9
+    return Array.from({ length: threeByThreeTotalSquares }).fill(null)
+  }
 
-  test() {
-    return 42
+  static update(board, position, mark) {
+    const leftHalfFinish = position - 1
+    const rightHalfStart = position
+
+    return [
+      ...board.slice(0, leftHalfFinish),
+      mark,
+      ...board.slice(rightHalfStart),
+    ]
+  }
+
+  static get(board, position) {
+    const zeroIndexedPosition = position - 1
+    return board[zeroIndexedPosition] || ''
+  }
+
+  static hasWinner(board) {
+    const winningMoves = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ]
+
+    const get = i => Board.get(board, i)
+
+    const hasWinningCombination = winningMoves.filter(
+      move =>
+        get(move[0]) != '' &&
+        get(move[0]) == get(move[1]) &&
+        get(move[1]) == get(move[2])
+    )
+
+    return hasWinningCombination.length > 0 ? true : false
+  }
+
+  static available(board) {
+    return board.reduce(
+      (available, currentSquare, index) =>
+        currentSquare != null ? available : available.concat(index + 1),
+      []
+    )
   }
 }
--- test/Board.test.js
+++ test/Board.test.js
@@ -1,8 +1,55 @@
 const Board = require('../src/Board')
 
 describe('The Board class', () => {
-  it('Knows the answer', () => {
-    const board = new Board()
-    expect(board.test()).toEqual(42)
+  describe('With Board.new()', () => {
+    it('Returns 9 moves by default', () => {
+      const board = Board.new()
+      expect(Board.available(board)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+  })
+
+  describe('Using Board.update()', () => {
+    it('Can retrieve a mark from a played square', () => {
+      const board = Board.new()
+      const updatedBoard = Board.update(board, 9, 'X')
+      expect(Board.get(updatedBoard, 9)).toEqual('X')
+    })
+
+    it('Returns 8 moves if one has been played', () => {
+      const board = Board.new()
+      const updatedBoard = Board.update(board, 1, 'X')
+      expect(Board.available(updatedBoard)).toEqual([2, 3, 4, 5, 6, 7, 8, 9])
+    })
+  })
+
+  describe('Using Board.hasWinner()', () => {
+    it('Knows an board with no played moves has no winner', () => {
+      const board = Board.new()
+      expect(Board.hasWinner(board)).toBe(false)
+    })
+
+    it('Knows three played moves on a horizontal is a winning move', () => {
+      const board = Board.new()
+      const firstMove = Board.update(board, 1, 'X')
+      const secondMove = Board.update(firstMove, 2, 'X')
+      const thirdMove = Board.update(secondMove, 3, 'X')
+      expect(Board.hasWinner(thirdMove)).toBe(true)
+    })
+
+    it('Knows three played moves on a vertical is a winning move', () => {
+      const board = Board.new()
+      const firstMove = Board.update(board, 2, 'X')
+      const secondMove = Board.update(firstMove, 5, 'X')
+      const thirdMove = Board.update(secondMove, 8, 'X')
+      expect(Board.hasWinner(thirdMove)).toBe(true)
+    })
+
+    it('Knows three played moves on a diagonal is a winning move', () => {
+      const board = Board.new()
+      const firstMove = Board.update(board, 3, 'X')
+      const secondMove = Board.update(firstMove, 5, 'X')
+      const thirdMove = Board.update(secondMove, 7, 'X')
+      expect(Board.hasWinner(thirdMove)).toBe(true)
+    })
   })
 })
