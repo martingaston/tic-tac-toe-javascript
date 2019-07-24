673d5aa0c7769ff40d943ede3af00304561c52a8
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-07-24 22:37:38 +0100

    add random computer ai

--- __test__/game/ai/random.test.js
+++ __test__/game/ai/random.test.js
@@ -1,7 +1,25 @@
-//const random = require('../../../src/game/ai/random')
+const random = require('../../../src/game/ai/random')
+const board = require('../../../src/game/board')
 
-describe('the random computer AI', () => {
-  it('is true', () => {
-    expect(true).toBe(true)
+describe('The random computer AI', () => {
+  it('picks the one remaining move if available from a board state', () => {
+    let state = board.newState()
+    state = board.update(state, 1, 'X')
+    state = board.update(state, 2, 'X')
+    state = board.update(state, 3, 'X')
+    state = board.update(state, 4, 'X')
+    state = board.update(state, 5, 'X')
+    state = board.update(state, 6, 'X')
+    state = board.update(state, 7, 'X')
+    state = board.update(state, 8, 'X')
+
+    expect(random.chooseMove(board, state)).toEqual(9)
+  })
+
+  it('does not pick an unavailable move', () => {
+    let state = board.newState()
+    state = board.update(state, 1, 'X')
+
+    expect(random.chooseMove(board, state)).not.toEqual(1)
   })
 })
--- src/game/ai/random.js
+++ src/game/ai/random.js
@@ -1,1 +1,11 @@
-module.exports = {}
+module.exports = {
+  chooseMove: (board, state) => {
+    const available = board.available(state)
+    const index = randomNumberGenerator(available.length)
+
+    return available[index]
+  },
+}
+
+const randomNumberGenerator = maxNumberExclusive =>
+  Math.floor(Math.random() * maxNumberExclusive)
